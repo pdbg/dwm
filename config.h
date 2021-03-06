cc712d64f49dac9fa6ed26e51dcfcce1761e169a
Prakash <code@bhagat.ml>
2021-03-06 13:39:01 -0500

    Added clipmenu shortcut

--- config.h
+++ config.h
@@ -219,6 +219,8 @@ static Key keys[] = {
 	{ MODKEY,			XK_b,		togglebar,	{0} },
 	/* { MODKEY|ShiftMask,		XK_b,		spawn,		SHCMD("") }, */
 	{ MODKEY,			XK_n,		spawn,		SHCMD(TERMINAL " -e nvim -c VimwikiIndex") },
+	{ ControlMask|ShiftMask,
+                        XK_v,		spawn,		SHCMD("clipmenu && xdotool type $(xsel --clipboard)") },
 	{ MODKEY|ShiftMask,	XK_n,		spawn,		SHCMD(TERMINAL " -e newsboat; pkill -RTMIN+6 dwmblocks") },
 	{ MODKEY,			XK_m,		spawn,		SHCMD(TERMINAL " -e ncmpcpp") },
 	{ MODKEY|ShiftMask,	XK_m,		spawn,		SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
