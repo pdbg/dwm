91cbac29ac0316f594070b1919670342fbc72775
pdbg <code@bhagat.ml>
2021-04-21 19:38:42 -0400

    Changed btm cmd

--- config.h
+++ config.h
@@ -170,7 +170,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_e,		spawn,		SHCMD(TERMINAL " -e neomutt ; pkill -RTMIN+12 dwmblocks; rmdir ~/.abook") },
 	{ MODKEY|ShiftMask,	XK_e,		spawn,		SHCMD(TERMINAL " -e abook -C ~/.config/abook/abookrc --datafile ~/.config/abook/addressbook") },
 	{ MODKEY,			XK_r,		spawn,		SHCMD(TERMINAL " -e lf") },
-	{ MODKEY|ShiftMask,	XK_r,		spawn,		SHCMD(TERMINAL " -e btm --basic") },
+	{ MODKEY|ShiftMask,	XK_r,		spawn,		SHCMD(TERMINAL " -e btm --basic --mem_as_value") },
 	{ MODKEY,			XK_t,		setlayout,	{.v = &layouts[0]} }, /* tile */
 	{ MODKEY|ShiftMask,	XK_t,		setlayout,	{.v = &layouts[1]} }, /* bstack */
 	{ MODKEY,			XK_y,		setlayout,	{.v = &layouts[2]} }, /* spiral */
